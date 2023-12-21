ac15136cdf1c3c86dd96df9cf97ae498868297b1
as9055 <154539882+as9055@users.noreply.github.com>
2023-12-21 21:36:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function song(){
+
+  console.log('ok')
+var audio = new Audio("moye moye pro.mp3");
+audio.play();
+audio.loop = true;
+
+}
+var button = document.getElementById("btn");
+
+window.onload =  function () {
+  
+    var marqueeText = "Moye Moye | ";
+    var repetitions = 5; // Replace with your repetitions
+  
+    var marqueeElements = document.getElementsByClassName("marquee-text");
+    // console.log('ok');
+    
+    for (var i = 0; i < marqueeElements.length; i++) {
+      for (var j = 0; j < repetitions; j++) {
+        marqueeElements[i].innerHTML += marqueeText;
+      }
+    }
+  
+
+    // Trigger a click event on the button
+    button.click();
+    
+  };
+  
+ 
+
+  
